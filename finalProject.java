4ae558af2f9ccfc476cd01d8558dcebcd9cf9913
Thomas McSorley <tmcsorley314@gmail.com>
2018-12-02 16:15:13 -0500

    Edit of beginGame Method
    beginGame method now initializes an array of enemy names and randomly selects one. Based on the first letter of the name, it displays a message with correct grammar ("a" versus "an"). Description alterations.

--- finalProject.java
+++ finalProject.java
@@ -23,8 +23,44 @@ public class finalProject {
   }
 
   public static void beginGame(){
-
+    System.out.println();
+    System.out.println("You have entered the arena...");
+    String[] enemies = new String[10];
+    nameBasicEnemies(enemies);
+    String enemyName = "" + enemies[(int)(Math.random() * 10)]; //Randomizes enemy name.
+    char c = enemyName.charAt(0);
+    try {
+      TimeUnit.SECONDS.sleep(1); //Waits 1 second.
+    }
+    catch (InterruptedException e) {
+      e.printStackTrace(); //Prints exception.
+    }
+    if (c=='A' || c=='E' || c=='I' || c=='O' || c=='U') {
+      System.out.println();
+      System.out.println("An " + enemyName + " approaches.");
+    }
+    else{
+      System.out.println();
+      System.out.println("A " + enemyName + " approaches.");
+    }
   }
+
+  /** initializeEnemyArray creates a string array of various enemy names to be
+  selected from.*/
+  public static String[] nameBasicEnemies(String[] enemies){
+    enemies[0] = "Goblin";
+    enemies[1] = "Skeleton";
+    enemies[2] = "Blob Creature";
+    enemies[3] = "Mutant Bat";
+    enemies[4] = "Banshee";
+    enemies[5] = "Shadow Creature";
+    enemies[6] = "Witch";
+    enemies[7] = "Ogre";
+    enemies[8] = "Evil Fairy";
+    enemies[9] = "Enchanted Armor";
+    return enemies;
+  }
+
 
   public static void quitGame(){
     System.exit(0);
@@ -39,7 +75,7 @@ public class finalProject {
       line = TextIO.getln();
       System.out.println(line);
       try {
-        TimeUnit.SECONDS.sleep(1); //Waits 2 seconds.
+        TimeUnit.SECONDS.sleep(1); //Waits 1 second.
       }
       catch (InterruptedException e) {
         e.printStackTrace(); //Prints exception.
